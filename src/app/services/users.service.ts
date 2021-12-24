e8d2943bdc8ce6409a8597a818dda1b3801d0d76
Tony Michael <tee.jhay1@gmail.com>
2021-12-24 22:03:05 +0100

    update

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -10,7 +10,7 @@ export class UsersService {
   constructor(private http: HttpClient) {}
 
   fetchUsers(): Observable<IUser[]> {
-    return this.http.get('http://localhost:3000/usersDB') as Observable<
+    return this.http.get('https://lend-sqr.herokuapp.com/users') as Observable<
       IUser[]
     >;
   }
